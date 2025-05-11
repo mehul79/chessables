e34cf7e5e68fcfb1bba8e19f2eab7fc9f6420473
mehul79 <mehulrgupta@gmail.com>
2025-05-11 13:46:44 +0530

    github login setup

--- backend_1/src/utils/passport.ts
+++ backend_1/src/utils/passport.ts
@@ -17,6 +17,8 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
+
+
 export function initPassport() {
   console.log("reached initPassport");
   if (
