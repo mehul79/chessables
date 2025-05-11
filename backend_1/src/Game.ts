b08417074d0571a0af41e063299e0418e7d21a26
mehul79 <mehulrgupta@gmail.com>
2025-05-11 11:54:52 +0530

    fixed file tree

--- backend_1/src/Game.ts
+++ backend_1/src/Game.ts
@@ -1,93 +1,101 @@
 import { Chess } from "chess.js";
-import WebSocket from "ws"; 
-import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
+import WebSocket from "ws";
+import { GAME_OVER, INIT_GAME, MOVE } from "./utils/messages";
 
 export class Game {
   public player1: WebSocket;
   public player2: WebSocket;
   private board: Chess;
   private startTime: Date;
-  
+
   constructor(player1: WebSocket, player2: WebSocket) {
     this.player1 = player1;
     this.player2 = player2;
     this.board = new Chess();
     this.startTime = new Date();
-    this.player1.send(JSON.stringify({
-      type: INIT_GAME,
-      playload: {
-        color: "white",
-        startTime: this.startTime
-      }
-    }));
-    
-    this.player2.send(JSON.stringify({
-      type: INIT_GAME,
-      playload: {
-        color: "black",
-      }
-    }));
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        playload: {
+          color: "white",
+          startTime: this.startTime,
+        },
+      })
+    );
+
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        playload: {
+          color: "black",
+        },
+      })
+    );
   }
-  
-  makeMove(socket: WebSocket, move: {from: string, to: string}){
+
+  makeMove(socket: WebSocket, move: { from: string; to: string }) {
     //validation here
-    //is this user valid 
+    //is this user valid
     //is the move valid
-    
-    if(this.board.turn() === "w" && socket != this.player1){
+
+    if (this.board.turn() === "w" && socket != this.player1) {
       console.log("Not white's turn");
       return;
     }
-    
-    if(this.board.turn() === "b" && socket != this.player2){
+
+    if (this.board.turn() === "b" && socket != this.player2) {
       console.log("Not black's turn");
       return;
     }
-    
-    
-    try{
+
+    try {
       this.board.move(move);
       console.log("Move made backend:: ", move);
-    }catch(e){
+    } catch (e) {
       console.log("ERROR hehahaha: ", e);
       return;
     }
-    
-    
-    if(this.board.isGameOver()){
+
+    if (this.board.isGameOver()) {
       console.log("brooo");
-      
-      this.player1.emit(JSON.stringify({
-        type: GAME_OVER,
-        payload: {
-          winner: this.board.turn() === "w"? "black" : "white"
-        }
-      })); 
-      
-      this.player2.emit(JSON.stringify({        
-        type: GAME_OVER,
-        payload: {
-          winner: this.board.turn() === "w"? "black" : "white"
-        }
-      })); 
-      
+
+      this.player1.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+
+      this.player2.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
     }
 
-    this.player1.send(JSON.stringify({
-      type: MOVE,
-      payload: move,
-    }))
-    this.player2.send(JSON.stringify({
-      type: MOVE,
-      payload: move,
-    }))
-    
-    
+    this.player1.send(
+      JSON.stringify({
+        type: MOVE,
+        payload: move,
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: MOVE,
+        payload: move,
+      })
+    );
+
     //update the board
     //push the move
-    
+
     //check if the game is over
-    
+
     //send the updated board to both the users
   }
 }
